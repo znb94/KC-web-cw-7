f9a8861571788807974b28ef7f559a16710ce790
Nancy El-Sharkawi <Nancy@Nancys-MacBook-Pro.local>
2020-12-23 19:54:52 +0300

    It's a test

---
+++ question1.js
@@ -0,0 +1,11 @@
+let person = "نانسي";
+
+function sayHello(name) {
+  //   if (name) {
+  return `مرحباً, ${name} 👋🏻`;
+  //   }
+  //   return `مرحباً 👋🏻`;
+}
+
+// console.log(sayHello(person));
+console.log(sayHello());
